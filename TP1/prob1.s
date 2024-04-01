570d3dea5747266565c51ceac6b11b64f58d95a0
André Lamego <andrelamegobh@hotmail.com>
2024-04-01 13:27:58 -0300

    TP1 - problem 1

--- TP1/prob1.s
+++ TP1/prob1.s
@@ -29,16 +29,17 @@ teste2: la a0, vetor2
 
 
 ##### R2 START MODIFIQUE AQUI START #####
-multiplos: add  t0, a0  , zero  # Endereço do vetor em t0 para iteração
-	   addi t5, zero, 4     # Carrega t5 com 4 para conseguir tamanho do vetor em bytes
-      	   mul  t5, a1  , t5    # Carrego em t5 o tamanho total do vetor em bytes
-           add  t5, a0  , t5    # Carrego endereço final do vetor em t5
-loop:	   lw   t1, 0(t0) 	    # Carrega valor do vetor em t1	
-           rem  t2, t1  , a2	# Carrega resto da divisão do número de t2 por a2
-           bne  t2, zero, else	# If(resto == 0){...}else{...}
-if:	   addi t3, t3  , 1     # Adiciona 1 em t3 para contagem dos múltiplos
-else:	   addi t0, t0  , 4     # Desloca resgistrador do vetor para próximo elemento
-   	   bne  t0, t5  , loop  # Confere se o vetor ja foi todo lido
+multiplos: add  t0  , a0  , zero  # Endereço do vetor em t0 para iteração
+	   addi t5  , zero, 4     # Carrega t5 com 4 para conseguir tamanho do vetor em bytes
+      	   mul  t5  , a1  , t5    # Carrego em t5 o tamanho total do vetor em bytes
+           add  t5  , a0  , t5    # Carrego endereço final do vetor em t5
+loop:	   lw   t1  , 0(t0) 	    # Carrega valor do vetor em t1	
+           rem  t2  , t1  , a2	# Carrega resto da divisão do número de t2 por a2
+           bne  t2  , zero, else	# If(resto == 0){...}else{...}
+if:	   addi t3  , t3  , 1     # Adiciona 1 em t3 para contagem dos múltiplos
+else:	   addi t0  , t0  , 4     # Desloca resgistrador do vetor para próximo elemento
+   	   bne  t0  , t5  , loop  # Confere se o vetor ja foi todo lido
+           jalr zero, 0(ra)
 ##### R2 END MODIFIQUE AQUI END #####
 
 FIM: addi t0, zero, a0
